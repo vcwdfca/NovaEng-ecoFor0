6d17987948722747cf2cb9c49e4f491d24e49bda
vcwdfca <vcwdfca@outlook.com>
2026-07-24 04:08:10 +0800

    环境的搭建

---
+++ src/main/java/com/vcwdfca/ecofor0/ecofor0.java
@@ -0,0 +1,18 @@
+package com.vcwdfca.ecofor0;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class ecofor0 {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+    }
+
+}
